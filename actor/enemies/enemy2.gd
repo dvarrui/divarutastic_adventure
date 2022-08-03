5e93314faa094a6e7076b5cab630c1f7dff19e4a
David Vargas <dvarrui@protonmail.com>
2022-08-03 21:45:27 +0100

    [U] enemy3.

--- actor/enemies/enemy2.gd
+++ actor/enemies/enemy2.gd
@@ -1,26 +1,31 @@
 extends Area2D
 
-export var speed = 200
+export var speed = 50
 export var x_min = -100
 export var x_max = 100
 export var direction = 1
 var origin = Vector2.ZERO
-var shape_offset = 5
 
 func _ready():
 	origin = position
 	$anim.play("default")
 	$anim.playing = true
+	$anim.flip_h = false
+	$anim.position.x = 3
 
 func _on_enemy2_body_entered(body):
 	if body.is_in_group("player"):
 		body.game_over()
 
 func _process(delta):
-	position.x += speed * direction * delta
-	$anim.flip_h = false
-	$shape.position.x = shape_offset * direction
 	if direction > 0:
+		position.x += speed * direction * delta
 		$anim.flip_h = true
+		$anim.position.x = -3
+	else:
+		position.x += speed * direction * delta
+		$anim.flip_h = false
+		$anim.position.x = 3
 	if position.x > (origin.x+x_max) or position.x < (origin.x+x_min):
+		# Cambio de dirección
 		direction = direction * (-1)
